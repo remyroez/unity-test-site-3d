22b4258a8390f64656ff81454fb290f2a5906173
Remy Roez <remyroez+github@gmail.com>
2017-07-29 23:49:20 +0900

    ✨ ワームの追加

--- Assets/Scripts/RemyRoez/JointCommand.cs
+++ Assets/Scripts/RemyRoez/JointCommand.cs
@@ -29,5 +29,31 @@ namespace RemyRoez
 			
 			rigidBody.AddForce(rigidBody.transform.forward * power);
 		}
+
+		public void Twist(int index, float power)
+		{
+			Joint joint = jointList[index];
+			if (joint == null) return;
+
+			Rigidbody rigidBody = joint.GetComponent<Rigidbody>();
+			if (rigidBody == null) return;
+
+			Rigidbody connectedBody = joint.connectedBody;
+			if (connectedBody == null) return;
+
+			Transform connected = connectedBody.transform;
+			if (connected == null) return;
+
+			joint.connectedBody = null;
+
+			Transform trans = joint.transform;
+
+			Vector3 bone = trans.position - connected.position;
+			Quaternion angle = Quaternion.AngleAxis(power, Quaternion.AngleAxis(90, Vector3.up) * bone);
+
+			trans.localRotation *= angle;
+
+			joint.connectedBody = connectedBody;
+		}
 	}
 }
